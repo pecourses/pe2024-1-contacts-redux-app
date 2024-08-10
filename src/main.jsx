aed0fbfc13e230b01ae84298f4db9950dcf3a835
Viktoriia <freshitpte@gmail.com>
2024-08-10 16:15:26 +0300

    Add App structure & styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App.jsx";
+import "./reset.css";
 import "./index.css";
+import App from "./App.jsx";
 import store from "./store";
 
 createRoot(document.getElementById("root")).render(
