f7379e9ddea6e453a97373472dc40b8fc27218ab
Viktoriia <freshitpte@gmail.com>
2024-08-11 18:48:03 +0300

    Refactop api

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './reset.css';
 import './index.css';
 import App from './App.jsx';
 import store from './store';
-// import axios from 'axios';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,48 +13,3 @@ createRoot(document.getElementById('root')).render(
     </Provider>
   </StrictMode>
 );
-
-// fetch('https://randomuser.me/api')
-//   .then(response => response.json())
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err))
-//   .finally(() => {});
-
-// async function getData () {
-//   try {
-//     const response = await fetch('http4s://randomuser.me/api');
-//     const data = await response.json();
-//     return data;
-//   } catch (err) {
-//     console.log(err);
-//   } finally {
-//   }
-// }
-
-// const res = await getData();
-// console.log(res);
-
-// Promise {status: (pending |fulfilled | rejected), result:}
-// get, post, patch/put, delete
-
-// const httpClient = axios.create({
-//   baseURL: 'https://jsonplaceholder.typicode.com',
-// });
-
-// // 'https://jsonplaceholder.typicode.com/posts'
-// try {
-//   const response = await httpClient.get('/posts/100');
-//   console.log(response.data);
-// } catch (err) {
-//   console.log(err);
-// }
-
-// httpClient
-//   .get('/posts')
-//   .then(response => console.log(response.data))
-//   .catch(err => console.log(err));
-
-// httpClient
-//   .get('/users')
-//   .then(response => console.log(response.data))
-//   .catch(err => console.log(err));
--- src/store/slices/postsSlice.js
+++ src/store/slices/postsSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import * as API from './../../api';
 
 const initialState = {
   posts: [],
@@ -7,43 +7,32 @@ const initialState = {
   error: null,
 };
 
-const httpClient = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com',
-});
-
 // action creator
 export const getPostsThunk = createAsyncThunk(
   'posts/getPosts',
   async (payload, thunkAPI) => {
     try {
-      const { data } = await httpClient.get('/posts');
+      const { data } = await API.getPosts();
       return data; // data => payload in 'posts/getPosts/fulfilled'
     } catch (err) {
       return thunkAPI.rejectWithValue(err.message); // err => payload in 'posts/getPosts/rejected'
     }
   }
 );
-console.dir(getPostsThunk);
-// pending: isFetching = true, error = null,
-// fulfilled: isFetching = false, posts = data
-// rejected: isFetching = false, error = err
+
 const postsSlice = createSlice({
   initialState,
   name: 'posts',
-  // (state,action)=>{}
   reducers: {},
   extraReducers: builder => {
-    // 'posts/getPosts/pending'
-    builder.addCase(getPostsThunk.pending, (state, { payload }) => {
+    builder.addCase(getPostsThunk.pending, state => {
       state.isFetching = true;
       state.error = null;
     });
-    // 'posts/getPosts/fulfilled'
     builder.addCase(getPostsThunk.fulfilled, (state, { payload }) => {
       state.isFetching = false;
       state.posts = payload;
     });
-    // 'posts/getPosts/rejected'
     builder.addCase(getPostsThunk.rejected, (state, { payload }) => {
       state.isFetching = false;
       state.error = payload;
