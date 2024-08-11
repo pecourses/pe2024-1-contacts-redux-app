6d61ca73d967ec13f617c9cfce080fba9c950438
Viktoriia <freshitpte@gmail.com>
2024-08-11 15:27:12 +0300

    Add async/await syntax

--- src/main.jsx
+++ src/main.jsx
@@ -13,3 +13,25 @@ createRoot(document.getElementById('root')).render(
     </Provider>
   </StrictMode>
 );
+
+fetch('https://randomuser.me/api')
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
+  .finally(() => {});
+
+async function getData () {
+  try {
+    const response = await fetch('http4s://randomuser.me/api');
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  } finally {
+  }
+}
+
+const res = await getData();
+console.log(res);
+
+// Promise {status: (pending |fulfilled | rejected), result:}
